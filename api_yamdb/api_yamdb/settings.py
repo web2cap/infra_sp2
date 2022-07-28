2365f085037ff8fac09dda467e1feae367b9218f
Pavel <web2cap@gmail.com>
2022-07-28 20:43:53 +0200

    fix db backend

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -61,9 +61,11 @@ WSGI_APPLICATION = "api_yamdb.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": os.getenv(
-            "DB_ENGINE", default="django.db.backends.postgresql_psycopg2"
+            # "DB_ENGINE", default="django.db.backends.postgresql_psycopg2"
+            "DB_ENGINE",
+            default="django.db.backends.postgresql",
         ),
-        "NAME": os.getenv("DB_NAME", default="postgresql"),
+        "NAME": os.getenv("DB_NAME", default="postgres"),
         "USER": os.getenv("POSTGRES_USER", default="postgres"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="postgres"),
         "HOST": os.getenv("DB_HOST", default="db"),
