0672f0d1ae7b463c953fd75e19aabee45d3d0067
Pavel <web2cap@gmail.com>
2022-07-28 20:33:34 +0200

    fix dbname

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -63,7 +63,7 @@ DATABASES = {
         "ENGINE": os.getenv(
             "DB_ENGINE", default="django.db.backends.postgresql_psycopg2"
         ),
-        "NAME": os.getenv("DB_NAME", default="postgres"),
+        "NAME": os.getenv("DB_NAME", default="postgresql"),
         "USER": os.getenv("POSTGRES_USER", default="postgres"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="postgres"),
         "HOST": os.getenv("DB_HOST", default="db"),
